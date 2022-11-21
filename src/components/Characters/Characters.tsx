7d914cf640f69360873445bd45dc80df6a25e5bf
henrikaag <55979072+henrikaag@users.noreply.github.com>
2022-11-21 12:00:04 +0100

    fix

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -10,34 +10,33 @@ import { MdComputer } from 'react-icons/md';
 const Characters = () => {
     return (
         <>
-<section className="container">
-        <h1>Characters</h1>
+        <section className="container">
+            <h1>Characters</h1>
 
-        <h3>Filter by platform</h3>
-        <Button variant="warning m-1">PC <MdComputer /></Button>
-        <Button variant="primary m-1">Playstation <FaPlaystation /></Button>
-        <Button variant="success m-1">Xbox <FaXbox /></Button>
-        <Button variant="danger m-1">Nintendo <SiNintendoswitch /></Button>
+            <h3>Filter by platform</h3>
+            <Button variant="warning m-1">PC <MdComputer /></Button>
+            <Button variant="primary m-1">Playstation <FaPlaystation /></Button>
+            <Button variant="success m-1">Xbox <FaXbox /></Button>
+            <Button variant="danger m-1">Nintendo <SiNintendoswitch /></Button>
 
+            <Row xs={1} md={2} className="g-4 mt-2">
+            {Array.from({ length: 4 }).map((_, idx) => (
+                <Col>
+                    <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                        This is a longer card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit
+                        longer.
+                        </Card.Text>
+                    </Card.Body>
+                    </Card>
+                </Col>
 
-        <Row xs={1} md={2} className="g-4 mt-2">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-
-        ))}
-      </Row>
+            ))}
+        </Row>
     </section>
       </>
     )
