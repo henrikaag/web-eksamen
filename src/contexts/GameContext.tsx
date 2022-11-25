0c23933523fd814460c64496899fb45dbab6ccff
MarkusPra <Markus.praner@outlook.com>
2022-11-25 10:15:11 +0100

    tedts

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -2,8 +2,13 @@ import { useEffect, useState, createContext, ReactNode } from "react";
 import IGameContext from "../interfaces/IGameContext";
 import IGame from "../interfaces/IGame";
 import GameService from "../services/GameService";
+import ICharacterContext from "../interfaces/ICharacterContext";
+import ICharacter from "../interfaces/ICharacter";
 
 export const GameContext = createContext<IGameContext | null>(null);
+
+export const CharacterContext = createContext<ICharacterContext | null>(null);
+
 
 type Props = {
     children: ReactNode
@@ -12,9 +17,12 @@ type Props = {
 const GameProvider = ({children} : Props) => {
 
     const [games, setGame] = useState<IGame[]>([]);
+    
+    const [characters, setCharacters] = useState<ICharacter[]>([]);
 
     useEffect(()=>{
         getGamesFromService();
+        getCharactersFromService();
     }, [])
 
     const getGamesFromService = async () => {
@@ -32,8 +40,23 @@ const GameProvider = ({children} : Props) => {
         await GameService.postGame(newGame);
     }
 
+    const getCharactersFromService = async () => {
+        const charactersFromService = await GameService.getAllCharacters();
+        setCharacters (charactersFromService);
+    }
+
+    const deleteCharactersById = async (id: number) => {
+        await GameService.deleteCharacter(id)
+        const newArray = characters.filter ( character => character.id != id)
+        setCharacters(newArray);
+    }
+
+    const addNewCharacter = async (newCharacter: ICharacter) => {
+        await GameService.postCharacter(newCharacter)
+    }
+
     return(
-        <GameContext.Provider value={{games, deleteGameById, addNewGame }}>
+        <GameContext.Provider value={{games, characters, deleteCharactersById, addNewCharacter, deleteGameById, addNewGame }}>
             {children}
         </GameContext.Provider>
     )
--- src/interfaces/IGameContext.ts
+++ src/interfaces/IGameContext.ts
@@ -1,9 +1,14 @@
+import DeleteCharacter from "../components/Characters/DeleteCharacter";
+import ICharacter from "./ICharacter";
 import IGame from "./IGame";
 
 interface IGameContext{
     games: IGame[];
+    characters: ICharacter[];
     deleteGameById: (id: number) => void;
     addNewGame: (newGame: IGame) => void;
+    deleteCharacterById: (id: number) => void;
+    addNewCharacter: (newCharacter: ICharacter) => void;
 }
 
 export default IGameContext;
